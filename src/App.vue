dc5c8c6f98575445e5ee2e592d121d442c3c8874
Asfak Ahmed ( rahi ) <asfakahmed680@gmail.com>
2025-01-28 17:28:22 +0600

    project initlize

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import MobileNavbar from "./components/MobileNavbar.vue";
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import AboutPlatform from "./components/AboutPlatform.vue";
+import OurWorks from "./components/OurWorks.vue";
+import WhatWeDo from "./components/WhatWeDo.vue";
+import WhyChooseUs from "./components/WhyChooseUs.vue";
+import Pricing from "./components/Pricing.vue";
+import Team from "./components/Team.vue";
+import Partnerships from "./components/Partnerships.vue";
+import Testimonials from "./components/Testimonials.vue";
+import InvestorMetrics from "./components/InvestorMetrics.vue";
+import Newsletter from "./components/Newsletter.vue";
+import Footer from "./components/Footer.vue";
+
+</script>
+
+<template>
+  <MobileNavbar/>
+  <Navbar/>
+  <Hero/>
+  <AboutPlatform/>
+  <OurWorks/>
+  <WhatWeDo/>
+  <WhyChooseUs/>
+  <Pricing/>
+  <Team/>
+  <Partnerships/>
+  <testimonials/>
+  <InvestorMetrics/>
+  <Newsletter/>
+  <Footer/>
+</template>
